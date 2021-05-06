9acfb7cf4e5db540f4dff1ca025a06acf789a3bd
jacksnorlax <62570505+jacksnorlax@users.noreply.github.com>
2021-05-06 10:48:55 +0200

    Skapade stack.js

---
+++ src/stack.js
@@ -0,0 +1,15 @@
+var _ require('underscore');
+
+let stack = [];
+
+exports.push = function (x) {
+  stack.push(x);
+};
+
+exports.pop = function () {
+  return stack.pop();
+};
+
+exports.peek = function () {
+  return stack[0];
+};
